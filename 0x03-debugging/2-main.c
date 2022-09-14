382d35cb3b6251e94b10fa5a3eaec86fe1688d4a
Kayode Halimat <halimatkayode30@gmail.com>
2022-09-14 07:30:19 -0700

    tried

--- 0x03-debugging/2-main.c
+++ 0x03-debugging/2-main.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include "main.h"
-#include "2-largest_number.c"
 
 /**
  * main - prints the largest of 3 integers
@@ -12,9 +11,9 @@ int main(void)
 	int a, b, c;
 	int largest;
 
-	a = 0;
+	a = 972;
 	b = -98;
-	c = 780;
+	c = 0;
 
 	largest = largest_number(a, b, c);
 
