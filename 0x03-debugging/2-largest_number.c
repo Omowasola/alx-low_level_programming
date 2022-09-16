999e57beadf309439f124e9cd9c7359186d59f66
Kayode Halimat <halimatkayode30@gmail.com>
2022-09-15 17:45:56 -0700

    Hope

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,20 +10,13 @@
 
 int largest_number(int a, int b, int c)
 {
-	int largest;
+	int largest = a;
 
-	if (a >= b && a >= c)
-	{
-		largest = a;
-	}
-	else if (b >= a && b >= c)
+	if (largest < b)
 	{
 		largest = b;
+		if (largest < c)
+			largest = c;
 	}
-	else
-	{
-		largest = c;
-	}
-
 	return (largest);
 }
