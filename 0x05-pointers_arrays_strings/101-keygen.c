f13001047871f386b5c3359183087a29ad06a480
Kayode Halimat <halimatkayode30@gmail.com>
2022-09-20 06:17:09 -0700

    advanced

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -6,11 +6,11 @@
  * main - generates keygen.
  * Return: 0 Always.
  */
-
 int main(void)
 {
-	int r = 0, c = 0;
+	int r = 0, c = 0;		
 	time_t t;
+	
 
 	srand((unsigned int) time(&t));
 	while (c < 2772)
@@ -18,7 +18,8 @@ int main(void)
 		r = rand() % 128;
 		if ((c + r) > 2772)
 			break;
-		c = c + r;
+
+		c = c + r
 		printf("%c", r);
 	}
 	printf("%c\n", (2772 - c));
